8969bdef80891cc68ccff157c2dddbc8dd227824
professionalowo <martin.brandl1307@gmail.com>
2025-12-09 20:49:28 +0100

    Simplify main function by removing unnecessary print statements and directly returning yyparse result

--- main.c
+++ main.c
@@ -3,11 +3,7 @@
 extern int yyparse(void);
 
 int main(int argc, char **argv) {
-    printf("Simple calculator (Flex + Bison)\n");
     printf("Enter expressions (e.g., 1+2*3)\n");
-    printf("Press Ctrl+D to exit\n");
     
-    yyparse();
-    
-    return 0;
+    return yyparse();
 }
